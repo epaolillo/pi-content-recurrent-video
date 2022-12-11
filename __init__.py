61a80e79aa2713927775368464c0c2af999c75bc
Ezequiel Paolillo <paolilloe@gmail.com>
2022-12-11 00:12:57 -0300

    Added construct

--- __init__.py
+++ __init__.py
@@ -6,5 +6,5 @@ from . import evaluation
 def detect(*args, **kwargs):
     return detector.detect(*args, **kwargs)
 
-def  construct_feature_vectors(url, feature_vectors_dir_name, feature_vector_function, framejump):
+def construct_feature_vectors(url, feature_vectors_dir_name, feature_vector_function, framejump):
     return featurevectors.construct_feature_vectors(url, feature_vectors_dir_name, feature_vector_function, framejump)
