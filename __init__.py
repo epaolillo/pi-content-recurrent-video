408e49a1aab05c0cfb69cea38ed90d7519994e2f
Ezequiel Paolillo <paolilloe@gmail.com>
2022-12-11 00:11:46 -0300

    Added construct_feature_vectors

--- __init__.py
+++ __init__.py
@@ -1,4 +1,10 @@
-from . import detector
+from . import detector, 
+from . import featurevectors
+from . import video_functions
+from . import evaluation
 
 def detect(*args, **kwargs):
     return detector.detect(*args, **kwargs)
+
+def  construct_feature_vectors(url, feature_vectors_dir_name, feature_vector_function, framejump):
+    return featurevectors.construct_feature_vectors(url, feature_vectors_dir_name, feature_vector_function, framejump)
