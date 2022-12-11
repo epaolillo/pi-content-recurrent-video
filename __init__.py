7a01cced95e946fcc33c35f8d806a5fce1319580
Ezequiel Paolillo <paolilloe@gmail.com>
2022-12-11 01:19:57 -0300

    Added missing fucntions

--- __init__.py
+++ __init__.py
@@ -11,3 +11,20 @@ def construct_feature_vectors(url, feature_vectors_dir_name, feature_vector_func
 
 def query_episodes_with_faiss(videos, vectors_dir):
     return detector.query_episodes_with_faiss(videos, vectors_dir)
+
+def get_framerate(video_fn):
+    return video_functions.get_framerate(video_fn)
+
+def fill_gaps(sequence, lookahead):
+    return detector.fill_gaps(sequence, lookahead)
+
+def get_two_longest_timestamps(timestamps):
+    return detector.get_two_longest_timestamps(timestamps)
+
+def to_time_string(seconds):
+    return detector.to_time_string(seconds)
+
+
+
+
+
