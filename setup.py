55cc431b0470f8d975b89f48293c42529bf4010c
Ezequiel Paolillo <paolilloe@gmail.com>
2022-12-10 22:47:16 -0300

    Added setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(name='Content recurrent detector',
+      version='0.1',
+      description='For detect content recurrent on videos',
+      url='https://github.com/epaolillo/pi-content-recurrent-video',
+      author='Ezequiel Paolillo',
+      author_email='paolilloe@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data = True,
+      install_requires=[
+          'Pillow==6.*',
+          'ffmpeg_python==0.*',
+          'matplotlib==3.*',
+          'opencv_python==4.*',
+          'pandas==0.*',
+          'scipy==1.*',
+          'tqdm==4.*',
+          'natsort==6.*',
+          'numpy==1.*',
+          'pytest==6.*'
+      ],
+      zip_safe=False)
